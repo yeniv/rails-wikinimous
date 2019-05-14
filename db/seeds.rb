eb6a7a6a75a7ed4348f84fae39595e2f92a777a6
Tristan Viney <yenivyeniv@gmail.com>
2019-05-14 22:58:08 +0200

    challenge 2 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |count|
+  puts "creating fake article #{count + 1} of 10!"
+  title = Faker::Book.title
+  content = Faker::Marketing.buzzwords
+  Article.create(title: title, content: content)
+end
